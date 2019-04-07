146d0fb1a6b904426e991131d60bab4429fca5b3
Kevin J Walters <kjw.github@gmail.com>
2019-04-07 12:15:25 +0100

    midi_intest1.py refresh, needed messages imported. #3

--- examples/midi_intest1.py
+++ examples/midi_intest1.py
@@ -2,14 +2,23 @@ import time
 import usb_midi
 import adafruit_midi
 
-### 0 is MIDI channel 1
+# A subset of messages/events
+# pylint: disable=unused-import
+from adafruit_midi.timing_clock            import TimingClock
+#from adafruit_midi.channel_pressure        import ChannelPressure
+from adafruit_midi.control_change          import ControlChange
+from adafruit_midi.note_off                import NoteOff
+from adafruit_midi.note_on                 import NoteOn
+from adafruit_midi.pitch_bend              import PitchBend
+
+
+# 0 is MIDI channel 1
 midi = adafruit_midi.MIDI(midi_in=usb_midi.ports[0], in_channel=0)
 
-print("Midi test II")
+print("Midi input test with pauses")
 
 # Convert channel numbers at the presentation layer to the ones musicians use
 print("Input channel:", midi.in_channel + 1 )
-print("Listening on input channel:", midi.in_channel + 1)
 
 # play with the pause to simulate code doing other stuff
 # in the loop
